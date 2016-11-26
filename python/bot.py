126a7ebd100e024c426d147a2f0301cecc460f29
educriado <ecriado95@gmail.com>
2016-11-26 18:03:48 +0200

    Testing without receiving messages is done.

--- python/bot.py
+++ python/bot.py
@@ -1,6 +1,7 @@
 from threading import Thread
 import requests
 import json
+import time
 
 calais_url = 'https://api.thomsonreuters.com/permid/calais'
 access_token = 'oNqeEfDTsuBOi4OIv9nm0sHWF9pcNrLw'
@@ -42,8 +43,10 @@ def api_call(bot):
     for topic in topics:
         if topic in bot.topics_count:
             bot.update_topic(topic)
+            print 'We update the topic: {}'.format(topic)
         else:
             bot.add_topic(topic)
+            print 'We add the topic: {}'.format(topic)
 
 
 def main():
@@ -51,12 +54,16 @@ def main():
     # the bot to return response.
     topics_count = {'cats': 1, 'sports': 1, 'music': 1}
     topic_files = [i + '.aiml' for i in topics_count]
-    conversation = ''
+    conversation = 'On September 28, 2015, karate was featured on a shortlist along with baseball, softball, skateboa' \
+                   'rding, surfing, and sport climbing to be considered for inclusion in the 2020 Summer Olympics. On' \
+                   ' June 1, 2016, the International Olympic Committees executive board announced they were supportin' \
+                   'g the inclusion of all five sports (counting baseball and softball as only one sport) for inclusi' \
+                   'on in the 2020 Games.'
     bot = Bot(topics_count, topic_files, conversation)
     # We won't call the API all the time
     count = 0
     test_count = 0
-    while test_count < 20:
+    while test_count < 10:
 
         # Read string
         print 'We have received a message.'
@@ -64,13 +71,15 @@ def main():
         count += 1
         if count % 5 == 0:
             # API call and topics - start new thread, it's too sloooow!
-            thread = Thread(target=api_call, args=(conversation, bot))
+            thread = Thread(target=api_call, args=(bot,))
             thread.start()
             count = 0
         # call brain for response 
         print 'Call for AIML response'
         # return message to main program
         print 'We send message back'
+        # Right now simulate that it takes time to receive a response
+        time.sleep(5)
 
 
 class Bot:
@@ -92,6 +101,17 @@ class Bot:
 
     def update_conversation(self, new_phrase):
         self.conversation.append(new_phrase)
+
+    def finish_conversation(self):
+        # Save conversation in file, save topics in file
+        f_conv = open('conversation.txt', 'w')
+        for item in bot.conversation.split():
+            f_conv.write(item)
+        f_conv.close()
+        f_topic = open('topics.txt', 'w')
+        for topic, count in bot.topics_count:
+            f_topic.write('{}, {}'.format(topic, count))
+        f_topic.close()
 
 
 if __name__ == '__main__':
