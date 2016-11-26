682ba49a09cc24777c7e5f56cf92618b85546b13
educriado <ecriado95@gmail.com>
2016-11-26 18:16:23 +0200

    Starting to create comunication between the 2 programs, using local sockets.

---
+++ python/com.py
@@ -0,0 +1,20 @@
+import socket
+
+HOST = ''
+PORT = 50007 # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print 'Connected by', addr
+while 1:
+    data = conn.recv(1024)
+    if not data: break
+    conn.sendall(data)
+conn.close()
+
+def receive_message():
+    return 1
+
+def send_message():
+    return 1
