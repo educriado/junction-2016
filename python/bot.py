202444a02ab870ecc19a7ce90f26b52bd04fca64
educriado <ecriado95@gmail.com>
2016-11-26 16:54:24 +0200

    Testing bot, not finished.

--- python/bot.py
+++ python/bot.py
@@ -2,15 +2,14 @@ from threading import Thread
 import requests
 import json
 
-
-
 calais_url = 'https://api.thomsonreuters.com/permid/calais'
 access_token = 'oNqeEfDTsuBOi4OIv9nm0sHWF9pcNrLw'
 headers = {
-    'X-AG-Access-Token' : access_token,
-    'Content-Type' : 'text/raw',
-    'outputformat' : 'application/json'
-    }
+    'X-AG-Access-Token': access_token,
+    'Content-Type': 'text/raw',
+    'outputformat': 'application/json'
+}
+
 
 def fetch_topics(response):
     # Look for topics values in the _typeGroup responses and add them to topics
@@ -25,55 +24,56 @@ def fetch_topics(response):
                 found_topics.append(value['name'])
     return found_topics
 
+
 def list_to_file(list, file_object):
     for item in list:
         file_object.write('{}\n'.format(item))
 
-def API_call(text, bot):
-    bot_conversation = bot.get_conversation()
+
+def api_call(bot):
+    # type: (object) -> object
     temp_file_object = open('/tmp/conv.txt', 'w+')
-    list_to_file(bot_conversation, temp_file)
+    list_to_file(bot.conversation, temp_file_object)
     files = {'tmp_file': temp_file_object}
     response = requests.post(calais_url, files=files, headers=headers,
                              timeout=80)
     temp_file_object.close()
     topics = fetch_topics(response)
-    existing_topics = bot.get_topics_count()
     for topic in topics:
-        if topic in existing_topics:
+        if topic in bot.topics_count:
             bot.update_topic(topic)
         else:
             bot.add_topic(topic)
-
 
 
 def main():
     # While true loop receiven messages from the main program and telling
     # the bot to return response.
     topics_count = {'cats': 1, 'sports': 1, 'music': 1}
-    topic_files  = [i + '.aiml' for i in topics_count]
-    bot = Bot(topics, files)
+    topic_files = [i + '.aiml' for i in topics_count]
+    conversation = ''
+    bot = Bot(topics_count, topic_files, conversation)
     # We won't call the API all the time
     count = 0
-    while True:
+    test_count = 0
+    while test_count < 20:
 
         # Read string
         print 'We have received a message.'
-        #text = read_message() # NOT IMPLEMENTED
+        # text = read_message() # NOT IMPLEMENTED
         count += 1
         if count % 5 == 0:
             # API call and topics - start new thread, it's too sloooow!
-            thread = Thread(target=API_call, args=(text, bot))
+            thread = Thread(target=api_call, args=(conversation, bot))
             thread.start()
             count = 0
         # call brain for response 
-
+        print 'Call for AIML response'
         # return message to main program
-        
+        print 'We send message back'
 
 
 class Bot:
-
     def __init__(self, topics, files, conversation):
         '''
         topics: initial list of topics (have to match previous AIML files)
@@ -82,12 +82,6 @@ class Bot:
         self.topics_count = topics
         self.files = files
         self.conversation = conversation
-
-    def get_topics_count(self):
-        return self.topics_count
-
-    def get_conversation(self):
-        return self.conversation
 
     def add_topic(self, topic_name):
         self.topics_count[topic_name] = 1
